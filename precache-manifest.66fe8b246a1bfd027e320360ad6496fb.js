2c0643a6301d97232034f8b87eab33628a53ee23
Zach Johnson <Zachjdev2121@gmail.com>
2020-09-18 07:22:16 -0400

    Updates

---
+++ precache-manifest.66fe8b246a1bfd027e320360ad6496fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d475c18632bdae83b17c48f1cd8f8a",
+    "url": "/Math-Trainer/index.html"
+  },
+  {
+    "revision": "65daf4dd76d9effc71a4",
+    "url": "/Math-Trainer/static/css/main.05aa9e24.chunk.css"
+  },
+  {
+    "revision": "d2c5ab91930ee72ac3b9",
+    "url": "/Math-Trainer/static/js/2.877db1d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Math-Trainer/static/js/2.877db1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65daf4dd76d9effc71a4",
+    "url": "/Math-Trainer/static/js/main.1fedd845.chunk.js"
+  },
+  {
+    "revision": "ce42f8cf1cef2df7e42a",
+    "url": "/Math-Trainer/static/js/runtime-main.c91fb3ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Math-Trainer/static/media/logo.5d5d9eef.svg"
+  }
+]);
